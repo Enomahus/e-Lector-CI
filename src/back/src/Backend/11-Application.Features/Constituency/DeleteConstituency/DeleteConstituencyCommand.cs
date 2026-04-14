a03092bf08e6a2203b167a28ac8f644a4c682829
Enomahus <enomahus@outlook.com>
2026-04-14 18:58:05 +0200

    Fix deteteconstituency

---
+++ src/back/src/Backend/11-Application.Features/Constituency/DeleteConstituency/DeleteConstituencyCommand.cs
@@ -0,0 +1,61 @@
+﻿using Application.Common.Enums;
+using Application.Exceptions;
+using Application.Models;
+using Application.Models.Errors;
+using FluentValidation;
+using Infrastructure.Persistence.Entities;
+using Infrastructure.Persistence.SQLServer.Contexts;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Pcea.Core.Net.Authorization.Application.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Tools.Logging;
+
+namespace Application.Features.Constituency.DeleteConstituency
+{
+    [WithPermission(nameof(AppPermission.DeleteConstituency))]
+    public class DeleteConstituencyCommand(long id) : IRequest<Result>
+    {
+        public long Id { get; set; } = id;
+    }
+
+    public class DeleteConstituencyCommandValidator : AbstractValidator<DeleteConstituencyCommand> 
+    {
+        private readonly ReadOnlyDbContext _context;
+        public DeleteConstituencyCommandValidator(ReadOnlyDbContext context)
+        {
+            _context = context;
+
+            RuleFor(c => c.Id).NotEmpty().WithMessage(ValidationErrorCode.Required.ToString())
+                .DependentRules(() =>
+                {
+                    RuleFor(c => c.Id)
+                    .MustAsync(
+                        (pollingStationId, token) =>
+                        _context.PollingStations.AnyAsync(ps => ps.Id == pollingStationId, token)
+                    )
+                    .WithMessage(ValidationErrorCode.PollingStationMustExist.ToString());
+                });
+        }
+    }
+
+    public class DeleteConstituencyCommandHandler(WritableDbContext context) : IRequestHandler<DeleteConstituencyCommand, Result>
+    {
+        public async Task<Result> Handle(DeleteConstituencyCommand command, CancellationToken cancellationToken)
+        {
+            using var activity = ActivitySourceLog.CQRS.Start().AddParameter(command, c => c.Id);
+
+            var constituencyDao = await context.Constituencies
+                .FirstOrDefaultAsync(c => c.Id == command.Id, cancellationToken)
+                ?? throw new NotFoundException(nameof(ConstituencyDao), command.Id);
+
+            context.Constituencies.Remove(constituencyDao);
+            await context.SaveChangesAsync(cancellationToken);
+
+            return Result.Default();
+
+        }
+    }
+}
---
+++ src/back/src/Backend/11-Application.Features/Constituency/DeleteConstituency/DeleteConstituencyController.cs
@@ -0,0 +1,38 @@
+﻿using Application.Api;
+using Application.Features.Users.DeleteUser;
+using Application.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Tools.Exceptions.Errors;
+
+namespace Application.Features.Constituency.DeleteConstituency
+{
+    [ExcludeFromCodeCoverage]
+    [ApiController]
+    [Route("constituency")]
+    [OpenApiTag("constituency")]
+    public class DeleteConstituencyController: ApiControllerBase
+    {
+        /// <summary>
+        /// Delete a constituency
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        [HttpDelete("{id}")]
+        [OpenApiOperation("DeleteConstituency", "Supprime une circonscription.", "")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(Result))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(Result<Error>))]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized, Type = typeof(Result<Error>))]
+        public Task<Result> DeleteConstituencyAsync(long id, CancellationToken cancellationToken)
+        {
+            var command = new DeleteConstituencyCommand(id);
+            return Mediator.Send(command, cancellationToken);
+        }
+    }
+}
