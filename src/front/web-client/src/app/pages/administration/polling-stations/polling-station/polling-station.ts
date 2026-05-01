6cc6c2c95b8d7ae52c6ae864cd9b376e0dd46828
Enomahus <Enomahus@outlook.com>
2026-05-01 20:38:07 +0200

    Fix

--- src/front/web-client/src/app/pages/administration/polling-stations/polling-station/polling-station.ts
+++ src/front/web-client/src/app/pages/administration/polling-stations/polling-station/polling-station.ts
@@ -1,15 +1,11 @@
 import { CommonModule } from '@angular/common';
+import { Component, computed, inject, input, OnInit, output, signal } from '@angular/core';
 import {
-  Component,
-  computed,
-  EventEmitter,
-  inject,
-  Input,
-  OnInit,
-  Output,
-  signal,
-} from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+  FormControl,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Breadcrumbs } from '@app/models/breadcrumb.model';
 import { ConstituencyNode } from '@app/models/constituency.model';
 import { ConstituencyApiService } from '@app/services/api/constituency.api.service';
@@ -23,10 +19,6 @@ import { ConstituencyTree } from '@app/shared/constituency-tree/constituency-tre
 import { Loader } from '@app/shared/loader/loader';
 import { StickyButtonsContainer } from '@app/shared/sticky-buttons-container/sticky-buttons-container';
 import { TranslateModule, TranslateService } from '@ngx-translate/core';
-import {
-  createPollingStationForm,
-  createPollingStationModelFromForm,
-} from './polling-station-form';
 
 @Component({
   selector: 'app-polling-station',
@@ -46,28 +38,50 @@ export class PollingStation implements OnInit {
   private readonly breadcrumbService = inject(BreadcrumbService);
   private readonly constituencyService = inject(ConstituencyApiService);
 
-  @Output() save = new EventEmitter<PollingStationModel>();
-  @Output() goBack = new EventEmitter<void>();
-  @Input() station?: PollingStationModel;
-  @Input() isToCreate = false;
-  @Input() isSaving = false;
+  save = output<PollingStationModel>();
+  goBack = output<void>();
+  pollingStation = input<PollingStationModel | undefined>(undefined);
+  initialSelectedId = input<number | undefined>(undefined);
+  isToCreate = input<boolean>(false);
+  isSaving = input<boolean>(false);
+  private fg = inject(NonNullableFormBuilder);
 
   nodes = signal<ConstituencyNode[]>([]);
   selectedNode = signal<ConstituencyNode | null>(null);
+  selectedNodes = signal<ConstituencyNode[]>([]);
+  initialTreeSelectedId = computed(() =>
+    !this.isToCreate() ? this.pollingStation()?.constituencyId : undefined,
+  );
 
   isSubmitting = signal(false);
 
-  form = createPollingStationForm();
+  form = this.fg.group({
+    stationNumber: new FormControl<string>('', {
+      validators: Validators.required,
+      nonNullable: true,
+    }),
+    wording: new FormControl<string>('', { validators: Validators.required, nonNullable: true }),
+    constituencyId: new FormControl<number | undefined>(undefined, {
+      validators: Validators.required,
+      nonNullable: true,
+    }),
+    isActive: new FormControl<boolean>(false, {
+      validators: Validators.required,
+      nonNullable: true,
+    }),
+  });
+
+  formData = signal<PollingStationModel | null>(null);
 
   readonly canSubmit = computed(() => this.form.valid && !this.isSubmitting());
 
   ngOnInit(): void {
-    this.setBreadcrumbs(this.station);
-    this.loadConstituencyTree();
+    this.setBreadcrumbs(this.pollingStation());
 
-    if (!this.isToCreate && this.station) {
-      this.form.patchValue(this.station);
+    if (!this.isToCreate() && this.pollingStation()) {
+      this.form.patchValue(this.pollingStation()!);
     }
+    this.loadConstituencyTree();
   }
 
   savePollingStation(): void {
@@ -76,9 +90,10 @@ export class PollingStation implements OnInit {
       return;
     }
     this.isSubmitting.set(true);
-    this.station = createPollingStationModelFromForm(this.form);
-    this.save.emit(this.station);
-    this.setBreadcrumbs(this.station);
+    const updateModel = this.form.getRawValue() as PollingStationModel;
+    this.formData.set(updateModel);
+    this.save.emit(this.formData()!);
+    this.setBreadcrumbs(this.formData()!);
   }
 
   setBreadcrumbs(station: PollingStationModel | undefined): void {
@@ -91,9 +106,9 @@ export class PollingStation implements OnInit {
         url: '/admin/polling-stations',
       },
       {
-        label: this.isToCreate
+        label: this.isToCreate()
           ? this.translateService.instant('pollingStation.newStationTitle')
-          : (station!.wording ?? ''),
+          : (station?.wording ?? ''),
       },
     ];
     this.breadcrumbService.setBreadcrumbs(breadcrumbs);
@@ -101,16 +116,19 @@ export class PollingStation implements OnInit {
 
   private loadConstituencyTree(): void {
     this.constituencyService.getConstituencyTree({}).subscribe((response) => {
-      const res = response.data;
-      if (res) {
-        const mappedNodes = res.map((c) => this.mapToNode(c));
-        this.nodes.set(mappedNodes);
+      const res = response.data ?? [];
 
-        if (this.station?.constituencyId) {
-          const found = this.findNodeById(mappedNodes, this.station.constituencyId);
-          if (found) {
-            this.selectedNode.set(found);
-          }
+      const nodes = res.map((c) => this.mapToNode(c));
+      if (!this.isToCreate() && this.pollingStation()?.constituencyId) {
+        this.expendPathToNode(nodes, this.pollingStation()!.constituencyId!);
+      }
+      this.nodes.set(nodes);
+
+      if (!this.isToCreate() && this.pollingStation()?.constituencyId) {
+        const parentNode = this.findNodeById(nodes, this.pollingStation()!.constituencyId!);
+        if (parentNode) {
+          this.selectedNodes.set([parentNode]);
+          this.selectedNode.set(parentNode);
         }
       }
     });
